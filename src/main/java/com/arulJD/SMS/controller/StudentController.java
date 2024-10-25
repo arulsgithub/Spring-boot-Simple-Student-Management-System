18d82fca9d6848eef2ce69889565a69901b6455a
arulsgithub <sarulmurugan06@gmail.com>
2024-10-25 19:32:49 +0530

    commit 2

--- src/main/java/com/arulJD/SMS/controller/StudentController.java
+++ src/main/java/com/arulJD/SMS/controller/StudentController.java
@@ -2,15 +2,11 @@ package com.arulJD.SMS.controller;
 
 import com.arulJD.SMS.entity.Student;
 import com.arulJD.SMS.service.StudentService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/students")
+@Controller
 public class StudentController {
 
     private final StudentService studentService;
@@ -20,14 +16,22 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    public ResponseEntity<Student> saveStudent(Student student){
-
-        return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.CREATED);
-    }
+//    public ResponseEntity<Student> saveStudent(Student student){
+//
+//        return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.CREATED);
+//    }
 
     @GetMapping("/students")
     public String listAllStudents(Model model){
         model.addAttribute("students", studentService.getAllStudents());
         return "students";
+    }
+
+    @GetMapping("/students/add")
+    public String addStudent(Model model){
+        Student student = new Student();
+        model.addAttribute("student", student);
+
+        return "addStudent";
     }
 }
